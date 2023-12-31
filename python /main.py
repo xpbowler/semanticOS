a91306d0ec8b53e6536e457f398d15a80785a20f
xpbowler <rnquan30@gmail.com>
2023-12-31 12:17:49 -0500

    added python scripts

---
+++ python /main.py
@@ -0,0 +1,16 @@
+import hnswlib
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import time
+import os
+import pickle
+from initialize import *
+from query import *
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+root_folder = '../'
+get_file_names(root_folder)
+
+query = ''
+print(search_for_query(query))
+
